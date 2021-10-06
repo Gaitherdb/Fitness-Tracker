fe3d0bc0a0485a3a794d575b8d34f0f68d4f5809
David Gaither <gaitherdb@gmail.com>
2021-10-05 20:08:52 -0400

    add morgan logger

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const workoutRoutes = require('./workout');
+const workoutRoutes = require('./workoutRoutes');
 // const resistanceRoutes = require('./resistanceRoutes');
 
 
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan");
 
 const PORT = process.env.PORT || 3000;
 
@@ -7,10 +8,10 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(logger("dev"));
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/cardio", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
