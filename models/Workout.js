66d71662839040b4eca971bc49d16539126d0664
David Gaither <gaitherdb@gmail.com>
2021-10-07 18:23:28 -0400

    getLastWorkout get request add sort - Success

--- models/Workout.js
+++ models/Workout.js
@@ -7,38 +7,35 @@ const workoutSchema = new Schema({
     type: Date,
     default: Date.now
   },
-  exercises: [
-    {
-      type: Array
-      
-      // {
-      //   type: String,
-      //   required: "Enter a name for type of exercise"
-      // },
-      // name: {
-      //   type: String,
-      //   required: "Enter a name for type of exercise"
-      // },
-      // duration: {
-      //   type: Number,
-      //   required: "Enter an amount of time"
-      // },
-      // distance: {
-      //   type: Number,
-      // },
-      // weight: {
-      //   type: Number,
-      // },
-      // reps: {
-      //   type: Number,
-      // },
-      // sets: {
-      //   type: Number,
-      // }
-    }
-  ],
+  exercises: [],
 
+ // {
+    //   type: {
+    //     type: String,
+    //     required: "Enter a name for type of exercise"
+    //   },
+    //   name: {
+    //     type: String,
+    //     required: "Enter a name for type of exercise"
+    //   },
+    //   duration: {
+    //     type: Number,
+    //     required: "Enter an amount of time"
+    //   },
+    //   distance: {
+    //     type: Number,
+    //   },
+    //   weight: {
+    //     type: Number,
+    //   },
+    //   reps: {
+    //     type: Number,
+    //   },
+    //   sets: {
+    //     type: Number,
 
+    //   }
+    // }
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
--- routes/api/workoutRoutes.js
+++ routes/api/workoutRoutes.js
@@ -4,80 +4,28 @@ const app = express();
 
 const db = require("../../models");
 
-//create workout
-router.post("/", ({ body }, res) => {
-  db.Workout.create(body)
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
+
 
 //getLastWorkout
 router.get("/", (req, res) => {
-  db.Workout.aggregate([{
-          $addFields: {
-              _id: "$_id",
-              day: "$day",
-              totalDuration: {
-                  $sum: "$exercises.duration"
-              }
-          }
+  db.Workout.aggregate([
+    {
+      $sort: { _id: 1 }
+    },
+    {
+      $addFields: {
+        day: "$day",
+        totalDuration: {
+          $sum: "$exercises.duration"
+        }
       }
+    }
 
   ], (err, data) => {
-      res.json(data);
+    res.json(data);
   });
 });
 
-//getWorkoutsInRange
-router.get("/range", (req, res) => {
-  db.Workout.find({})
-    .sort({ day: -1 })
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
 
-
-//add exercise
-router.put("/:id", ({ params }, res) => {
-  db.Workout.update(
-    {
-      _id: mongojs.ObjectId(params.id)
-    },
-    {
-      // $set: {
-      //   read: true
-      // }
-    },
-
-    (error, edited) => {
-      if (error) {
-        console.log(error);
-        res.send(error);
-      } else {
-        console.log(edited);
-        res.send(edited);
-      }
-    }
-  );
-});
-
-//delete exercise
-router.delete("/:id", (req, res) => {
-  Workout.remove({}, (error, response) => {
-    if (error) {
-      res.send(error);
-    } else {
-      res.send(response);
-    }
-  });
-});
 
 module.exports = router;
