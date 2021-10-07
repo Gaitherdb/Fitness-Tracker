86fd0b25f0e8bda9a931db8c1d1732d3ecd9e442
David Gaither <gaitherdb@gmail.com>
2021-10-07 19:31:42 -0400

    add path.join to express.static in server

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger("dev"));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
